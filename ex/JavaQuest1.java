9826947ec481d26b7916c512ca14a7eb40df8b01
nataterri <nata.terri@gmail.com>
2023-11-23 00:13:00 +0800

    updated

--- ex/JavaQuest1.java
+++ ex/JavaQuest1.java
@@ -4,50 +4,52 @@ public class JavaQuest1 {
 
     System.out.println("I am going to add two numbers together:");
     int first = 31;
+    // declare the second one
     int second = 42;
 
-    System.out.println("The first one: " + first); // 31
-    System.out.println("The second one: " + second); // 42
+    System.out.println("The first one: " + first); 
+    System.out.println("The second one: " + second);
 
     int sum = first + second;
-    System.out.println("The sum is " + sum); // 73
+    System.out.println("The sum is " + sum);
 
     // subtraction
     int subtract = second - first; 
-    System.out.println("The subtraction reuslt is " + subtract); // 11
+    System.out.println("The subtraction reuslt is " + subtract);
 
     // another sum
     byte b1 = 60;
     byte b2 = 70;
     sum = b1 + b2;
-    System.out.println("The another sum reuslt is " + sum); // 130
+    System.out.println("The another sum reuslt is " + sum);
 
     // declare two boolean variable with true and false value
     boolean varB = false;
     boolean varC = true;
     
-    System.out.println("The variable varB is " + varB); // False
-    System.out.println("The variable varC is " + varC); // True
+    System.out.println("The variable varB is " + varB); 
+    System.out.println("The variable varC is " + varC);
 
     // Reading Example
     float fnumber = -130.20f; // By default, numbers are treated as double in Java.
     // Use suffix 'f' with value to make it float.
     double dnumber = -20.50;
-    System.out.println("The variable fnumber is " + fnumber); // -130.2
-    System.out.println("The variable dnumber is " + dnumber); // -20.5
+    System.out.println("The variable fnumber is " + fnumber);
+    System.out.println("The variable dnumber is " + dnumber);
 
     // Find reminder. Declare variable a & reminder
     int a = 7;
     int b = 3;
     // Fix it
-    int reminder = 7 % 3; 
-    System.out.println("The variable reminder is " + reminder); // 1
+    int reminder = a % b; 
+    System.out.println("The variable reminder is " + reminder);
 
     // declare a variable x and assign value to it.
     int e = 7;
     int o = 8;
     int y = 4;
     int x = 2;
+    
     int result = e - o * x / y;
     System.out.println("The result is " + result);
   }
